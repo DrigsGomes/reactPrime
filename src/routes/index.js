45f53b900076bf79a711066369fc066acc3967ca
DrigsGomes <rodrigo.vitor169@gmail.com>
2022-05-03 23:24:52 -0300

    update.index

--- src/routes/index.js
+++ src/routes/index.js
@@ -25,8 +25,37 @@ function Routes(){
 
         }}
         >
-            <Drawer.Screen name="HomeDrawer" component={StackRoutes} />
-            <Drawer.Screen name="Movies" component={Movies} />
+            <Drawer.Screen 
+            name="HomeDrawer" 
+            component={StackRoutes} 
+            options={{
+                title:'Home',
+
+                drawerIcon: ({focused, size, color}) => (
+                    <MaterialCommunityIcons
+                    name={ focused ? 'movie-open' : 'movie-outline'}
+                    size={size}
+                    color={color}
+                    />
+                )
+            }}
+            />
+            <Drawer.Screen 
+            name="Movies" 
+            component={Movies} 
+            options={{
+                title: 'Meus filmes',
+
+                drawerIcon: ({focused, size, color}) => (
+                    <MaterialCommunityIcons
+                    name={ focused ? 'archive' : 'archive-outline'}
+                    size={size}
+                    color={color}
+                    />
+                )
+
+            }}
+            />
         </Drawer.Navigator>
     )
 }
