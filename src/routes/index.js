4dcc90ceab7c3847843250cc79903b0e051a9b5c
DrigsGomes <rodrigo.vitor169@gmail.com>
2022-05-03 23:07:26 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,31 @@
 import react from "react";
-
 import { createDrawerNavigator} from '@react-navigation/drawer';
 
-import Home from '../pages/Home';
+import { MaterialCommunityIcons} from '@expo/vector-icons';
+
 import Movies from '../pages/Movies';
+import StackRoutes from "./stackRoutes";
 
 const Drawer = createDrawerNavigator();
 
 function Routes(){
     return(
-        <Drawer.Navigator>
-            <Drawer.Screen name="Home" component={Home} />
+        <Drawer.Navigator
+        screenOptions={{
+            headerSown: false,
+
+            drawerStyle:{
+                backgroundColor: '#090A0E',
+                paddingTop:20,
+            },
+
+            drawerActiveBackgroundColor: '#E72F49',
+            drawerActiveTintColor: '#FFF',
+            drawerInactiveTintColor: '#FFF'
+
+        }}
+        >
+            <Drawer.Screen name="HomeDrawer" component={StackRoutes} />
             <Drawer.Screen name="Movies" component={Movies} />
         </Drawer.Navigator>
     )
