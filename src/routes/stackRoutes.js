69ba21cab60cc7a8bc2a25958f34d84b6a7ae347
DrigsGomes <rodrigo.vitor169@gmail.com>
2022-05-03 22:39:17 -0300

    Upadete.stackes.js

--- src/routes/stackRoutes.js
+++ src/routes/stackRoutes.js
@@ -1,20 +1,15 @@
 import React from "react";
-import {createDrawerNavigator} from '@react-navigation/drawer';
-
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Home from "../pages/Home";
-import Movies from "../pages/Movies";
-import { DevSettings } from "react-native";
-import { StackRouter } from "@react-navigation/routers";
 
-const Drawer = createDrawerNavigator();
+const Stack = createNativeStackNavigator();
 
-function StackRouter(){
+function StackRoutes(){
     return(
-        <Drawer.Navigator>
-            <Drawer.Screen name="Home" component={Home}/>
-            <Drawer.Screen name="Movies" component={Movies}/>
-        </Drawer.Navigator>
+        <Stack.Navigator>
+            <Stack.Screen name="Home" component={Home}/>
+        </Stack.Navigator>
     )
 }
 
-export default StackRouter;
+export default StackRoutes;
