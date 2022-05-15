0f5f23925a9f0f22823b66dd8b2d3c688c06b683
DrigsGomes <rodrigo.vitor169@gmail.com>
2022-05-15 20:46:33 -0300

    install axios

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -52,6 +52,26 @@ function Home(){
                 
                 />
 
+                <Title>Populares</Title>
+
+                <SliderMovie
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}
+                data={1,2,3,4}
+                renderItem={({item}) => <SliderItem/> }
+                
+                />
+
+                <Title>Mais votados</Title>
+
+                <SliderMovie
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}
+                data={1,2,3,4}
+                renderItem={({item}) => <SliderItem/> }
+                
+                />
+
             </ScrollView>
 
 
