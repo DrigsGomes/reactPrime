d16dcf707b82ea1279a0b9d0e593cb155c047c32
DrigsGomes <rodrigo.vitor169@gmail.com>
2022-05-21 21:15:56 -0300

    update.SliderItem.index.js

--- src/components/SliderItem/index.js
+++ src/components/SliderItem/index.js
@@ -11,21 +11,21 @@ import {
 
 import {Ionicons} from '@expo/vector-icons'
 
-function SliderItem({ item }){
+function SliderItem({ data }){
     return(
         <Container activeOpacity={0.7}> 
 
             <BannerItem
-                source={{uri:'https://unsplash.com/photos/atsUqIm3wxo'}}
+                source={{uri:`https://image.tmdb.org/t/p/original/${data.poster_path}` }}
             />
 
 
-            <Title numberOfLines={1}>Vigadores</Title>
+            <Title numberOfLines={1}>{data.title}</Title>
 
             <RateContainer>
                 <Ionicons name="md-star" size={12} color="#E7A74E"/>
-                <Rate>9/10</Rate>
-            </RateContainer>
+                <Rate>{data.vote_average}/10</Rate>
+            </RateContainer> 
         </Container>
     )
 }
