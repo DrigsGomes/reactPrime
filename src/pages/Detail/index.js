0b1cbd8edfddfeaf32eadfe66b59667e937c48d7
DrigsGomes <rodrigo.vitor169@gmail.com>
2022-05-23 21:08:19 -0300

    Create.Detail.js

---
+++ src/pages/Detail/index.js
@@ -0,0 +1,15 @@
+import react from "react";
+
+import { Container, Tilte} from "./styles";
+
+
+function Detail(){
+    return(
+        <Container>
+            <Tilte> Test </Tilte>
+
+        </Container>
+    )
+}
+
+export default Detail; 
--- src/routes/stackRoutes.js
+++ src/routes/stackRoutes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Home from "../pages/Home";
+import Detail from "../pages/Detail";
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +13,15 @@ function StackRoutes(){
             component={Home}
             options={{
                 headerShown:false
+            }}
+            />
+
+            <Stack.Screen
+            name="Detail"
+            component={Detail}
+            options={{
+                headerShown: false,
+                title: "Detalhes"
             }}
             />
         </Stack.Navigator>
