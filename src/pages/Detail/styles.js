8efe258f567abb116555adbfda79007a5285d199
DrigsGomes <rodrigo.vitor169@gmail.com>
2022-05-25 21:56:25 -0300

    Update.Datail.styles.js

--- src/pages/Detail/styles.js
+++ src/pages/Detail/styles.js
@@ -2,9 +2,28 @@ import styled from "styled-components/native";
 
 export const Container = styled.View`
     flex:1;
-    background-color: #191A30
+    background-color: #191A30;
 `;
 
-export const Tilte = styled.Text`
-
+export const Header = styled.View`
+    z-index: 99;
+    position: absolute;
+    top:5px;
+    width:100%;
+    display:flex;
+    flex-direction:row;
+    justify-content: space-between;
+    padding: 0 14px;
 `;
+
+export const HeaderButton = styled.TouchableOpacity`
+    width:46px;
+    height:46px;
+    background-color: rgba(255,255,255,0.8);
+    border-radius:23px;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const
+
